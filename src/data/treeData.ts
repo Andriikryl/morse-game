1ebf5eba17b76c36af9d1c67289fd3290c2db2fa
Andriikryl <andreyk557@gmail.com>
2024-05-12 18:43:02 +0300

    add treeData.ts

---
+++ src/data/treeData.ts
@@ -0,0 +1,102 @@
+export const treeData = [
+    {
+      title: "A (.-)",
+      branch: [
+        {
+          title: "B (-...)",
+          branch: [
+            {
+              title: "D (-..)",
+              branch: [
+                {
+                  title: "H (....)",
+                  branch: [
+                    {
+                      title: "Q (--.-)",
+                    },
+                    {
+                      title: "R (.-.)",
+                    },
+                  ],
+                },
+                {
+                  title: "I (..)",
+                  branch: [
+                    {
+                      title: "S (...)",
+                    },
+                    {
+                      title: "T (-)",
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              title: "E (.)",
+              branch: [
+                {
+                  title: "J (.---)",
+                  branch: [
+                    {
+                      title: "O (---)",
+                    },
+                    {
+                      title: "P (.--.)",
+                    },
+                  ],
+                },
+                {
+                  title: "K (-.-)",
+                  branch: [
+                    {
+                      title: "U (..-)",
+                    },
+                    {
+                      title: "V (...-)",
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          title: "C (-.-.)",
+          branch: [
+            {
+              title: "F (..-.)",
+              branch: [
+                {
+                  title: "L (.-..)",
+                  branch: [
+                    {
+                      title: "W (.--)",
+                    },
+                    {
+                      title: "X (-..-)",
+                    },
+                  ],
+                },
+                {
+                  title: "M (--)",
+                },
+              ],
+            },
+            {
+              title: "G (--.)",
+              branch: [
+                {
+                  title: "Y (-.--)",
+                },
+                {
+                  title: "Z (--..)",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ];
+  
