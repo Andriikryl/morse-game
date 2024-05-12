05d44eef736f5b531634c1bf0f9aef6577336e31
Andriikryl <andreyk557@gmail.com>
2024-05-12 18:40:59 +0300

    add HeroInfo component

---
+++ src/components/hero/heroInfo/HeroInfo.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./style.module.css";
+
+export default function HeroInfo() {
+  return (
+    <div>
+      <p>
+        We created this trainer to make the process of learning Morse code more
+        fun and to encourage people to keep at it. Give it a try if you’ve set
+        up Morse code for Gboard and are ready to learn Morse.
+      </p>
+      <p>
+        This experiment is part of a larger project to support Morse code for
+        accessible communication. Learn more about it at g.co/morse.
+      </p>
+      <p>Built by Use All Five and Google Creative Lab.</p>
+    </div>
+  );
+}
