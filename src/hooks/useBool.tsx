cb2a7fc0a2f8fb980dc908db8861ec59698cc51f
Andriikryl <andreyk557@gmail.com>
2024-05-12 15:10:18 +0300

    add useBool hook

---
+++ src/hooks/useBool.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+type Handlers = {
+  off: () => void;
+  on: () => void;
+  reset: () => void;
+  toggle: () => void;
+};
+
+export default function useBool(
+  initialState: boolean = false
+): [boolean, Handlers] {
+  if (typeof initialState !== "boolean") {
+    throw new Error("`initialState` argument must be a boolean");
+  }
+
+  const [state, setState] = React.useState<boolean>(initialState);
+
+  const handlers = React.useMemo(
+    () => ({
+      on: () => {
+        setState(true);
+      },
+      off: () => {
+        setState(false);
+      },
+      reset: () => {
+        setState(initialState);
+      },
+      toggle: () => {
+        setState((s) => !s);
+      },
+    }),
+    [initialState]
+  );
+
+  return [state, handlers];
+}
