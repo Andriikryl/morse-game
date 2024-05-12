27678067031197289e24a93c39e4b7dc2c170342
Andriikryl <andreyk557@gmail.com>
2024-05-12 18:41:24 +0300

    add DragModal component to the Hero component

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,8 +1,17 @@
+"use client";
 import React from "react";
 import styles from "./style.module.css";
 import { Heading } from "../heading/Heading";
+import useBool from "@/hooks/useBool";
+import { DragModal } from "../dragModal/DragModal";
+import { Button } from "../button/Button";
+import VisuallyHidden from "../visuallyHidden/VisuallyHidden";
+import HeroInfo from "./heroInfo/HeroInfo";
+import Question from "@/icons/Question";
+import Link from "next/link";
 
 export default function Hero() {
+  const [isOpen, handlers] = useBool(false);
   return (
     <section className={styles.hero}>
       <div className={styles.hero__box}>
@@ -13,7 +22,17 @@ export default function Hero() {
           I created this trainer to make the process of learning Morse code more
           fun and to encourage people to keep at it.
         </p>
+        <Link href={"/alphabet"}>alfabet</Link>
       </div>
+      <Button onClick={handlers.toggle} className={styles.info}>
+        <>
+          <VisuallyHidden>Info</VisuallyHidden>
+          <Question />
+        </>
+      </Button>
+      <DragModal open={isOpen} setOpen={handlers.toggle}>
+        <HeroInfo />
+      </DragModal>
     </section>
   );
 }
